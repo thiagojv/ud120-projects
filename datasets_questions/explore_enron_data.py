06b1082a73fdb996f67852cefa8c153fe8aacedb
Thiago Junqueira <thiagojv@gmail.com>
2018-01-06 02:26:22 -0200

    Refactoring and Exercise 27

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,56 +16,34 @@
 """
 
 import pickle
-import pandas as pd
+import math
 
 enron_data = pickle.load(open("final_project/final_project_dataset.pkl", "r"))
 
 # How many data points (people) are in the dataset?
-poi = list(enron_data.keys())
-print(len(poi))
+print("1 -> " + str(len(enron_data.keys())))
 
 # For each data point (person) how many features are available?
-count_features = 0
-for k, v in enron_data.items():
-    if len(v) > count_features:
-        count_features = len(v)
-print(count_features)
+print("2 -> " + str(len(enron_data.values()[0].keys())))
 
 # How many POIs are in the dataset?
 # data[person_name]["poi"]==1
-count_poi = 0
-for k, v in enron_data.items():
-    if v["poi"] == 1:
-        count_poi += 1
-print(count_poi)
+print("3 -> " + str(len(list(v for v in enron_data.values() if v["poi"] == 1))))
 
 # What is the total value of the stock belonging to James Prentice?
-fpj = enron_data["PRENTICE JAMES"]
-spj = pd.Series(fpj)
-print(spj)
-print(fpj["total_stock_value"])
+print("4 - PRENTICE JAMES total_stock_value -> " + str(enron_data["PRENTICE JAMES"]["total_stock_value"]))
 
 # How many email messages do we have from Wesley Colwell to persons of interest?
-fcl = enron_data["COLWELL WESLEY"]
-scw = pd.Series(fcl)
-print(scw)
-print(fcl["from_this_person_to_poi"])
+print("5 - COLWELL WESLEY from_this_person_to_poi -> " + str(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]))
 
 # What’s the value of stock options exercised by Jeffrey K Skilling?
-fskj = enron_data["SKILLING JEFFREY K"]
-sskj = pd.Series(fskj)
-print(sskj)
-print(fskj["exercised_stock_options"])
+print("6 - SKILLING JEFFREY K exercised_stock_options -> " + str(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]))
 
 # Of these three individuals (Lay, Skilling and Fastow), who took home the most money (largest value of “total_payments” feature)?
-flk = enron_data["LAY KENNETH L"]
-slk = pd.Series(flk)
-print(slk)
+print("7 - SKILLING JEFFREY K total_payments -> " + str(enron_data["SKILLING JEFFREY K"]["total_payments"]))
+print("7 - LAY KENNETH L total_payments -> " + str(enron_data["LAY KENNETH L"]["total_payments"]))
+print("7 - FASTOW ANDREW S total_payments -> " + str(enron_data["FASTOW ANDREW S"]["total_payments"]))
 
-ffa = enron_data["FASTOW ANDREW S"]
-sfa = pd.Series(ffa)
-print(sfa)
-
-print(fskj["total_payments"])
-print(flk["total_payments"])
-print(ffa["total_payments"])
+# How many folks in this dataset have a quantified salary? Known email address?
+print("8 -> " + str(len(list(v for v in enron_data.values() if math.isnan(float(v["salary"])) == 0))))
+print("8 -> " + str(len(list(v for v in enron_data.values() if v["email_address"] != "NaN"))))
