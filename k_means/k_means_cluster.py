31ffbfb647d6d55a03a48db2e48c4fa127435a46
Thiago Junqueira <thiagojv@gmail.com>
2018-02-07 23:16:34 -0200

    K-means Mini Project

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.cluster import KMeans
 
 
 
@@ -43,27 +44,37 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
-
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+def max_min(feature):
+    feature_list = list(v[feature] for v in data_dict.values() if not numpy.isnan(float(v[feature])))
+    print numpy.max(feature_list)
+    print numpy.min(feature_list)
+
+max_min(feature_2)
+max_min(feature_1)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
+#for f1, f2, _ in finance_features:
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+kmeans = KMeans(n_clusters=2).fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 
 
